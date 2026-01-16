b8197b447a179d851c773078f6fe3a41f56e9301
KIKODER <159853177+KIKODER@users.noreply.github.com>
2026-01-15 21:29:20 -0600

    Created functions to set a mark at an index, get the board, and reset the board.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const boardArray = [null, null, null, null, null, null, null, null, null];
 
-turnFill = prompt("Enter a number between 0-8:");
+function setMark(index, mark){
+    return boardArray[index] = mark;
+}
 
-boardArray[turnFill] = 'X';
+function getBoard() {
+    console.log(boardArray);
+}
 
-console.log(boardArray);
+function reset(){
+    for (let i = 0; i < boardArray.length; i++) {
+        boardArray[i] = null;
+    }
+}
+
+boardIndex = prompt("Enter a number between 0-8:");
+playerMark = prompt("Enter your mark 'X' or 'O':");
+
+setMark(boardIndex, playerMark);
+getBoard();
+
+reset();
+getBoard();
