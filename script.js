41b717bd667230144446ea1670fe43344ca8bfea
KIKODER <159853177+KIKODER@users.noreply.github.com>
2026-01-16 19:29:20 -0600

    Added functions to check if the player's entered mark is valid, and gives the remaining mark to the second player.

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const boardArray = [null, null, null, null, null, null, null, null, null];
 
 function setMark(index, mark){
     return boardArray[index] = mark;
+    //later, add function to refuse if cell isn't empty//
 }
 
 function getBoard() {
+    return boardArray;
+}
+
+function logBoard() {
     console.log(boardArray);
 }
 
@@ -14,16 +19,45 @@ function reset(){
     }
 }
 
-const player = {
-    name: "",
-    mark: ""
-};
+function createPlayer(name, mark) {
+    return {
+        name: name,
+        mark: mark
+    }
+}
 
-player.name = prompt("Enter your name:");
-player.mark = prompt("Enter your mark 'X' or 'O':");
+function getValidMark(promptText) {
+    let mark = prompt(promptText);
+
+    while (mark === null || !isValidMark(mark)) {
+        mark = prompt("Invalid mark. Please enter 'X' or 'O':");
+    }
+    return mark.trim().toUpperCase();
+}
+
+function isValidMark(mark) {
+    const cleaned = mark.trim().toUpperCase();
+    return cleaned === "X" || cleaned === "O";
+}
+
+function switchMark(mark) {
+    return mark === "X" ? "O" : "X";
+}
+
+const name1 = prompt("Enter player 1's name:");
+const mark1 = getValidMark("Enter player 1's mark ('X' or 'O'):");
+
+const name2 = prompt("Enter player 2's name:");
+const mark2 = switchMark(mark1);
+
+const player1 = createPlayer(name1, mark1);
+const player2 = createPlayer(name2, mark2);
 
 boardIndex = prompt("Enter a number between 0-8:");
 
-setMark(boardIndex, player.mark);
-getBoard();
+setMark(boardIndex, player1.mark);
+logBoard();
+
+console.log(player1.mark);
+console.log(player2.mark);
 
